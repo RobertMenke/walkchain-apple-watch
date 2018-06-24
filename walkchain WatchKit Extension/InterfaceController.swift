02e16dfaee418c07f8e15f07f7d1b0974be52012
Robert Menke <robert.b.menke@gmail.com>
2018-06-24 13:09:16 -0400

    initial commit

--- walkchain WatchKit Extension/InterfaceController.swift
+++ walkchain WatchKit Extension/InterfaceController.swift
@@ -8,24 +8,68 @@
 
 import WatchKit
 import Foundation
-
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
 
+    //======================
+    // IB Outlets
+    //======================
+    @IBOutlet var stepsLabel: WKInterfaceLabel!
+    @IBOutlet var distanceLabel: WKInterfaceLabel!
+    
+    //======================
+    // Instance variables
+    //======================
+    let pedometer = CMPedometer()
+    
+    //======================
+    // Lifecycle methods
+    //======================
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
     
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        createPedometerListener()
     }
     
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        pedometer.stopUpdates()
+        //TODO: Send update to server or background processing queue
     }
-
+    
+    //======================
+    // Pedometer Events
+    //======================
+    private func createPedometerListener() {
+        if CMPedometer.isPaceAvailable() {
+            pedometer.startUpdates(from: Date()) { (data, err) in
+                if let data = data {
+                    self.updateSteps(data: data)
+                    self.updateDistance(data: data)
+                }
+            }
+        }
+    }
+    
+    private func updateSteps(data : CMPedometerData) {
+        //We always want a whole, positive number of steps
+        let steps = data.numberOfSteps.uintValue
+        stepsLabel.setText(String(format: "%lu Steps", steps))
+    }
+    
+    private func updateDistance(data : CMPedometerData){
+        //Distance is in meters
+        if let distance = data.distance {
+            let milesWalked = metersToMiles(meters: distance)
+            distanceLabel.setText(String(format: "%.2f Miles", milesWalked))
+        }
+    }
+    
+    private func metersToMiles(meters : NSNumber) -> Double {
+        return meters as! Double / 1609.344
+    }
 }
